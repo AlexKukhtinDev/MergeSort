b88f39cdd4eeadefb1b2acc9e4712d2e2e8070b8
Alex Kukhtin <alex.kukhtin.dev@gmail.com>
2019-02-11 22:07:13 +0300

    Correct code

--- src/com/kukhtin/MergeSort.java
+++ src/com/kukhtin/MergeSort.java
@@ -1,8 +1,7 @@
 package com.kukhtin;
 
 public class MergeSort {
-    static void merge(int array[], int start, int mid, int end)
-    {
+    public static void merge(int array[], int start, int mid, int end) {
         int start2 = mid + 1;
         if (array[mid] <= array[start2]) {
             return;
@@ -10,8 +9,7 @@ public class MergeSort {
         while (start <= mid && start2 <= end) {
             if (array[start] <= array[start2]) {
                 start++;
-            }
-            else {
+            } else {
                 int value = array[start2];
                 int index = start2;
 
@@ -27,7 +25,8 @@ public class MergeSort {
             }
         }
     }
-    static void mergeSort(int arr[], int left, int right) {
+
+    public static void mergeSort(int arr[], int left, int right) {
         if (left < right) {
             int m = left + (right - left) / 2;
 
@@ -37,18 +36,19 @@ public class MergeSort {
             merge(arr, left, m, right);
         }
     }
-    static void printArray(int A[], int size)
-    {
+
+    public static void printArray(int array[], int size) {
         int i;
         for (i = 0; i < size; i++)
-            System.out.print(A[i] + " ");
+            System.out.print(array[i] + " ");
         System.out.println();
     }
+
     public static void main(String[] args) {
-        int array[] = { 12,14,15,8,9,23,45,10,5,6,99,41 };
+        int[] array = {12, 14, 15, 8, 9, 23, 45, 10, 5, 6, 99, 41};
         int arr_size = array.length;
 
         mergeSort(array, 0, arr_size - 1);
-        printArray(array, arr_size) ;
+        printArray(array, arr_size);
     }
 }
